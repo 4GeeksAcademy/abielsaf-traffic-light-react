f8eaf7764664a5ac4cbbf56762928c8716783496
Ángel Bielsa <145146806+abielsaf@users.noreply.github.com>
2024-01-12 16:51:25 +0000

    Modified background image in CSS, missing buttons functionality

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
+	const [ selectedColor, setSelectedColor] = useState("red");
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="traffic-light-container"> 
+				<div className="traffic-light-cable"></div>
+				<div className="traffic-light ">
+					<div
+						onClick={() => setSelectedColor("red")}  
+						className={"light red" + ((selectedColor === "red") ? " glow" : (""))}>
+					</div>
+					<div
+						onClick={() => setSelectedColor("yellow")}   
+						className={"light yellow" + ((selectedColor === "yellow") ? " glow" : (""))}>
+					</div>
+					<div
+						onClick={() => setSelectedColor("green")}   
+						className={"light green" + ((selectedColor === "green") ? " glow" : (""))}>
+					</div>
+				</div>
+
+			</div>
 		</div>
 	);
 };
